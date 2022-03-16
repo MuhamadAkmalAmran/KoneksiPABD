7732e9b56c5cf8b23e2b3d907e539155ab442dfb
MuhamadAkmalAmran <akmal.amran52@gmail.com>
2022-03-16 14:11:53 +0700

    Add project files.

---
+++ KoneksiPABD/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace KoneksiPABD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Connecting();
+        }
+
+        public void Connecting()
+        {
+            using (
+                SqlConnection con = new SqlConnection("data source=LENOVO\\AKMALAMRAN;database=ProdiTI;User ID=sa;Password=uzumaki")
+                )
+            {
+                con.Open();
+                Console.WriteLine("Berhasil terhubung ke database!");
+            }    
+            
+        }
+    }
+}
